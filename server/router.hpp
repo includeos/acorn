0f0c62c5e57e4fbf2d7d09f372e14e289c023e64
RicoAntonioFelix <ricoantoniofelix@yahoo.com>
2016-06-28 14:41:23 -0400

    Augmented file router.hpp

--- server/router.hpp
+++ server/router.hpp
@@ -18,277 +18,302 @@
 #ifndef ROUTER_HPP
 #define ROUTER_HPP
 
-#include <functional>
 #include <regex>
 #include <stdexcept>
+#include <functional>
+
+/* Local Dependencies */
 #include "request.hpp"
 #include "response.hpp"
 
 namespace server {
 
-  //-------------------------------
-  // This class is used to provide
-  // route resolution
-  //-------------------------------
+  /**
+   * @brief This class is used to describe
+   * route resolution
+   *
+   * Its main function is to register information
+   * relating request to response mappings
+   */
   class Router {
   private:
-    //-------------------------------
-    // Internal class type aliases
-    //-------------------------------
+    /* Internal Alias Section */
+
+    /*
+     * This entity is the pattern matching mechanism used to
+     * map a requested resource to its prefered representation
+     */
     using Route_expr = std::regex;
-    using Callback = std::function<void(Request_ptr, Response_ptr)>;
+
+    /*
+     * This entity is used to generate a representation of the
+     * requested resource
+     */
+    using Generator = std::function<void(Request_ptr, Response_ptr)>;
+
+    /*
+     * An entity to encapsulate the pattern matching mechanism and
+     * the representation generator into a single unit for the
+     * purpose of registration
+     */
     struct Route {
-
       Route_expr expr;
-      Callback callback;
+      Generator  generator;
 
-      Route(const char* ex, Callback c) : expr{ex}, callback{c}
-      {}
-
+      /*
+       * Constructor
+       *
+       * |param| <ex> : A requested resource
+       *
+       * |param| <g> : A resource generator
+       */
+      Route(const char* ex, Generator g) : expr{ex}, generator{g} {}
     };
 
-    using Route_table = std::unordered_map< http::Method, std::vector<Route> >;
+    /*
+     * An entity that's used for registering request to response mappings
+     */
+    using Route_table = std::unordered_map<http::Method, std::vector<Route>>;
+
+    /*
+     * This entity is used as a latch that refer to external information which
+     * implies that it always depends on an external entity
+     */
     using Span = gsl::span<char>;
 
   public:
-    //-------------------------------
-    // Default constructor to set up
-    // default routes
-    //-------------------------------
+    /**
+     * @brief Default constructor that creates an empty registration
+     * table
+     */
     explicit Router() = default;
 
-    //-------------------------------
-    // Default destructor
-    //-------------------------------
+    /**
+     * @brief Default destructor
+     */
     ~Router() noexcept = default;
 
-    //-------------------------------
-    // Default move constructor
-    //-------------------------------
+    /**
+     * @brief Default move constructor
+     */
     Router(Router&&) = default;
 
-    //-------------------------------
-    // Default move assignment operator
-    //-------------------------------
+    /**
+     * @brief Default move assignment operator
+     */
     Router& operator = (Router&&) = default;
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {OPTIONS} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_options(Routee&& route, Callback result);
+    Router& on_options(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {GET} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_get(Routee&& route, Callback result);
+    Router& on_get(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {HEAD} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_head(Routee&& route, Callback result);
+    Router& on_head(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {POST} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_post(Routee&& route, Callback result);
+    Router& on_post(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {PUT} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_put(Routee&& route, Callback result);
+    Router& on_put(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {DELETE} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_delete(Routee&& route, Callback result);
+    Router& on_delete(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {TRACE} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_trace(Routee&& route, Callback result);
+    Router& on_trace(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {CONNECT} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_connect(Routee&& route, Callback result);
+    Router& on_connect(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Add a route mapping for route
-    // resolution upon request
-    //
-    // @tparam (std::string) route - The route to map unto a
-    //                               resulting destination
-    //
-    // @param result - The route mapping
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Register a route for the {PATCH} HTTP method
+     *
+     * @tparam {std::string} route : A pattern for a requested
+     * resource
+     *
+     * @param generator : The representation generator
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee>
-    Router& on_patch(Routee&& route, Callback result);
+    Router& on_patch(Routee&& route, Generator generator);
 
-    //-------------------------------
-    // Install a new route table for
-    // route resolutions
-    //
-    // @tparam (http::Router) new_routes - The new route table
-    //                                     to install
-    //
-    // @return - The object that invoked this method
-    //-------------------------------
+    /**
+     * @brief Install a new route table for route resolutions
+     *
+     * @tparam {http::Router} new_routes : The new route table
+     * to install
+     *
+     * @return The object that invoked this method
+     */
     template <typename Routee_Table>
     Router& install_new_configuration(Routee_Table&& new_routes);
 
-
     /**
-     * Get the route callback where Route_expr matched a given path
+     * @brief Get the generator for the pattern macthed resource
      *
-     * @param path : the route path
-     * @note : not const becuase it uses index operator to a map
+     * @param method  : An HTTP method
+     * @param pattern : The pattern to match in the registry
+     *
+     * @note : This method is not declared |const| because it utilizes
+     * the subscript operator from std::map
      **/
-    inline Callback match(http::Method, const std::string&);
+    Generator match(const http::Method method, const std::string& pattern);
 
   private:
-
-    Router(const Router&) = delete;
-    Router& operator = (const Router&) = delete;
-
     Route_table route_table_;
 
-
+    /*
+     * Prohibit copy operations
+     */
+    Router(const Router&) = delete;
+    Router& operator = (const Router&) = delete;
   }; //< class Router
 
-
+  /**
+   * @brief This class is used to represent errors that occur while
+   * using the provided {Router} operations
+   */
   class Router_error : public std::runtime_error {
     using runtime_error::runtime_error;
   };
 
-
   /**--v----------- Implementation Details -----------v--**/
 
   template <typename Routee>
-  inline Router& Router::on_options(Routee&& route, Callback result) {
-    route_table_[http::OPTIONS].emplace_back({std::forward<Routee>(route), result});
+  inline Router& Router::on_options(Routee&& route, Generator generator) {
+    route_table_[http::OPTIONS].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
   template <typename Routee>
-  inline Router& Router::on_get(Routee&& route, Callback result) {
-    route_table_[http::GET].emplace_back(std::forward<Routee>(route),  result);
+  inline Router& Router::on_get(Routee&& route, Generator generator) {
+    route_table_[http::GET].emplace_back(std::forward<Routee>(route),  generator);
     return *this;
   }
 
   template <typename Routee>
-  inline Router& Router::on_head(Routee&& route, Callback result) {
-    route_table_[http::HEAD].emplace_back(std::forward<Routee>(route), result);
+  inline Router& Router::on_head(Routee&& route, Generator generator) {
+    route_table_[http::HEAD].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
   template <typename Routee>
-  inline Router& Router::on_post(Routee&& route, Callback result) {
-    route_table_[http::POST].emplace_back(std::forward<Routee>(route), result);
+  inline Router& Router::on_post(Routee&& route, Generator generator) {
+    route_table_[http::POST].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
   template <typename Routee>
-  inline Router& Router::on_put(Routee&& route, Callback result) {
-    route_table_[http::PUT].emplace_back(std::forward<Routee>(route), result);
+  inline Router& Router::on_put(Routee&& route, Generator generator) {
+    route_table_[http::PUT].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
 
   template <typename Routee>
-  inline Router& Router::on_delete(Routee&& route, Callback result) {
-    route_table_[http::DELETE].emplace_back(std::forward<Routee>(route), result);
+  inline Router& Router::on_delete(Routee&& route, Generator generator) {
+    route_table_[http::DELETE].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
   template <typename Routee>
-  inline Router& Router::on_trace(Routee&& route, Callback result) {
-    route_table_[http::TRACE].emplace_back(std::forward<Routee>(route), result);
+  inline Router& Router::on_trace(Routee&& route, Generator generator) {
+    route_table_[http::TRACE].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
   template <typename Routee>
-  inline Router& Router::on_connect(Routee&& route, Callback result) {
-    route_table_[http::CONNECT].emplace_back(std::forward<Routee>(route), result);
+  inline Router& Router::on_connect(Routee&& route, Generator generator) {
+    route_table_[http::CONNECT].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
   template <typename Routee>
-  inline Router& Router::on_patch(Routee&& route, Callback result) {
-    route_table_[http::PATCH].emplace_back(std::forward<Routee>(route), result);
+  inline Router& Router::on_patch(Routee&& route, Generator generator) {
+    route_table_[http::PATCH].emplace_back(std::forward<Routee>(route), generator);
     return *this;
   }
 
@@ -298,25 +323,23 @@ namespace server {
     return *this;
   }
 
-
-  inline Router::Callback Router::match(http::Method method, const std::string& path) {
-
+  inline Router::Generator Router::match(const http::Method method, const std::string& path) {
     auto routes = route_table_[method];
-    if (routes.empty())
-      throw Router_error("No routes for method " + http::method::str(method));
 
-    for (auto& route : routes)
+    if (routes.empty()) {
+      throw Router_error("No routes for method " + http::method::str(method));
+    }
+
+    for (auto& route : routes) {
       if (std::regex_match(path.begin(), path.end(), route.expr))
         return route.callback;
+    }
 
     throw Router_error("No matching route for " + http::method::str(method) + " " + path);
   }
 
-
-
   /**--^----------- Implementation Details -----------^--**/
 
 } //< namespace server
-
 
 #endif //< ROUTER_HPP
